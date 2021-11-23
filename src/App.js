34c4320e5b3aa1a2c87f0ca5545b54d96171fefb
BrendonLH <Brendonhampton1988@gmail.com>
2021-11-23 14:51:34 -0800

    basic styling for main page, added how to play section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import PlayerForm from "./pages/mainPage/playerForm.js";
 import "./pages/mainPage/main.scss";
+import HowTo from "./pages/mainPage/howToPlay.js";
 
 function App() {
   return (
     <div className="App">
-      <p>Escape Room</p>
-      <PlayerForm />
+      <h1>Escape Room</h1>
+      <PlayerForm className="playerForm" />
+      <HowTo className="howTo" />
     </div>
   );
 }
