77f85df033933331f5b692666e7fb2c1e9ebe506
BrendonLH <Brendonhampton1988@gmail.com>
2021-11-30 13:12:35 -0800

    initial commit

--- src/pages/gamePage/game.js
+++ src/pages/gamePage/game.js
@@ -1,4 +1,7 @@
+import { useSelector } from "react-redux";
 export default function Game() {
+  const game = useSelector((state) => state.game);
+  console.log(game, "game page state");
   return (
     <div>
       <h1>Game Page</h1>
