2d87aa4df62d45cdffcc71d039ad766b15ea283d
BrendonLH <Brendonhampton1988@gmail.com>
2021-12-02 14:14:40 -0800

    initial commit

--- src/pages/gamePage/game.js
+++ src/pages/gamePage/game.js
@@ -1,12 +1,16 @@
 import { useSelector } from "react-redux";
 export default function Game() {
   const game = useSelector((state) => state.game);
-  console.log(game, "game page state");
-  // const playerName = game.playerName;
-  // const riddle = game.riddles;
+  const riddle = game.riddles;
+  console.log(riddle.riddlesArr);
+
+  // having issue with async await
+
   return (
     <div>
       <h1>Game Page</h1>
+      {/* {<p>{riddle}</p>} */}
+      <input type="text" placeholder="your answer" />
     </div>
   );
 }
