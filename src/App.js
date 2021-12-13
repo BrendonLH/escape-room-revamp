cd21116d23bf62b16cf9c36f228a2af709614c42
BrendonLH <Brendonhampton1988@gmail.com>
2021-12-13 14:32:13 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import HowTo from "./pages/mainPage/howToPlay.js";
 function App() {
   return (
     <div className="App">
-      <div className="ghost">
-        <div className="eye"></div>
-        <div className="eye"></div>
-        <div className="mouth"></div>
-      </div>
-
-      <div className='app-title'>
+      <div className="app-title">
         <h1 id="main-title">Escape Room</h1>
         <h2 id="main-subtitle">Revamp</h2>
       </div>
