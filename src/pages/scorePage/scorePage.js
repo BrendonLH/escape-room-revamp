d4d013427ba79236cd4c67876d88c3ff4c0840b9
BrendonLH <Brendonhampton1988@gmail.com>
2021-12-15 15:28:56 -0800

    initial commit

--- src/pages/scorePage/scorePage.js
+++ src/pages/scorePage/scorePage.js
@@ -1,12 +1,17 @@
 import { useSelector } from "react-redux";
+import "./scorePage.scss";
 
 export default function ScorePage() {
-    const finalScore = useSelector((state) => state.game);
-    console.log(finalScore);
-    return (
-        <div>
-            <h2>{finalScore.playerName.charAt(0).toUpperCase() + finalScore.playerName.slice(1)}'s Score</h2>
-            <h3>{finalScore.score}</h3>
-        </div>
-    )
+  const finalScore = useSelector((state) => state.game);
+  console.log(finalScore);
+  return (
+    <div className="ScorePage">
+      <h2 className="player-score-name">
+        {finalScore.playerName.charAt(0).toUpperCase() +
+          finalScore.playerName.slice(1)}
+        's Score
+      </h2>
+      <h3 className="final-score">{finalScore.score}</h3>
+    </div>
+  );
 }
