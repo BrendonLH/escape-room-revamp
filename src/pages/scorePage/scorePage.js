d5ae5310ee1377329820ba4b48cdd803e13efc5e
BrendonLH <Brendonhampton1988@gmail.com>
2021-12-17 15:09:38 -0800

    initial commit

--- src/pages/scorePage/scorePage.js
+++ src/pages/scorePage/scorePage.js
@@ -2,16 +2,31 @@ import { useSelector } from "react-redux";
 import "./scorePage.scss";
 
 export default function ScorePage() {
-  const finalScore = useSelector((state) => state.game);
-  console.log(finalScore);
+  const gameEnd = useSelector((state) => state.game);
+  console.log(gameEnd);
+  // const userPostURL = "https://escape-room-server.herokuapp.com/userPost";
+
+  // send userscore to the DB
+
+  // fetch(userPostURL, {
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //     playerName: gameEnd.playerName,
+  //     score: gameEnd.score
+  //   }),
+  //   headers: {
+  //     "Content-type": "application/json; charset=UTF-8"
+  //   }
+  // });
+
   return (
     <div className="ScorePage">
       <h2 className="player-score-name">
-        {finalScore.playerName.charAt(0).toUpperCase() +
-          finalScore.playerName.slice(1)}
+        {gameEnd.playerName.charAt(0).toUpperCase() +
+          gameEnd.playerName.slice(1)}
         's Score
       </h2>
-      <h3 className="final-score">{finalScore.score}</h3>
+      <h3 className="final-score">{gameEnd.score}</h3>
     </div>
   );
 }
